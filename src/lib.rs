0162afbced940b512d8727860a27e08a07e199a4
1140982 <ryan.e.mason@raytheon.com>
2023-05-23 17:04:54 -0500

    3.7.3.2 pg.47

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
 //! lib.rs
-use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
+use actix_web::web;
+use actix_web::{App, HttpResponse, HttpServer};
 use std::net::TcpListener;
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
@@ -9,10 +20,12 @@ async fn health_check() -> HttpResponse {
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
-    App::new()
-    .route("/health_check", web::get().to(health_check))
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
     Ok(server)
-    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-//! src/main.rs 
+//! src/main.rs
 
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0")
-    .expect("Failed to bind to random port.");
- 
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port.");
+
     zero2prod::run(listener)?.await
 }
